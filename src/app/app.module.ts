109d1282eb21c789545562339e749aee2ac2dd04
MonTea-lh <haouch.lahcen@gmail.com>
2017-12-01 17:03:07 +0100

    Adding connection + inscription routes / connection form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { ConnectionComponent } from './authentication/connection/connection.component';
 import { InscriptionComponent } from './authentication/inscription/inscription.component';
+import { routes } from './authentication/authentication.route';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ConnectionComponent,
-    InscriptionComponent
+    InscriptionComponent,
+
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/authentication/authentication.route.ts
@@ -0,0 +1,17 @@
+
+
+
+import { Route } from '@angular/router';
+import { ConnectionComponent } from './connection/connection.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+
+export const routes: Route[] = [
+  {
+    path: 'connection',
+    component: ConnectionComponent
+  },
+  {
+    path: 'inscription',
+    component: InscriptionComponent
+  }
+];
