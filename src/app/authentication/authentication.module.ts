3b3847d96b568673d228031c7e13b1a5f8ba38b8
MonTea-lh <haouch.lahcen@gmail.com>
2017-12-01 13:32:50 +0100

    Generating connection + inscription components

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { ConnectionComponent } from './connection/connection.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [InscriptionComponent, ConnectionComponent]
 })
 export class AuthenticationModule { }
